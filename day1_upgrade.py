a73268e423699f04c078d4164e0159c9d1b0f544
Timothy Mckibbens <mckibbenst@gmail.com>
2026-02-06 17:30:49 -0500

    Refactor summary into function

---
+++ day1_upgrade.py
@@ -0,0 +1,13 @@
+def print_company_summary(name, customers, revenue, profitable):
+    print("Company Name:", name)
+    print("Customers:", customers)
+    print("Revenue:", revenue)
+    print("Profitable:", profitable)
+
+
+print_company_summary(
+    "My First Repo",
+    10,
+    500.00,
+    True
+)
